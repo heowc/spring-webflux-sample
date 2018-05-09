a4581a1b4f2c73691e4f91f00ebe797f15f4e4ba
heowc <heowc1992@gmail.com>
2018-05-09 16:05:55 +0900

    Add: [spring-webflux] - add test dependency - add sample

--- src/test/java/com/heowc/SimpleTest.java
+++ src/test/java/com/heowc/SimpleTest.java
@@ -2,19 +2,25 @@ package com.heowc;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
-
-import static com.heowc.Constant.LIST;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 public class SimpleTest extends AbstractStopWatchTest {
 
     @Test
-    public void simple() {
-        Flux.fromIterable(LIST)
-                .map(String::toUpperCase)
-                .subscribe(
-                        System.out::println,
-                        Throwable::printStackTrace,
-                        () -> System.out.println("Done")
-                );
+    public void testAppendBoomError() {
+        Flux<String> source = Flux.just("foo", "bar");
+
+        StepVerifier.create(appendBoomError(source))
+                .expectNext("foo") // onNext
+                .expectNext("bar")
+//                .expectComplete() // onComplete
+                .expectErrorMessage("boom") // onError
+
+                .verify();
+    }
+
+    private <T> Flux<T> appendBoomError(Flux<T> source) {
+        return source.concatWith(Mono.error(new IllegalArgumentException("boom")));
     }
 }
